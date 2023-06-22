632764bf8f869e7438baf63c7ee1fff2a239a20d
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-22 08:45:57 +0530

    Create commands.py

---
+++ AutoPost/plugins/commands.py
@@ -0,0 +1,13 @@
+# (c) @TheLx0980
+
+from pyrogram import filters, Client
+
+
+@Client.on_message(filters.command("start") & filters.private & filters.incoming)
+async def start(client, message):
+    await message.reply(
+        text="I'm Online",
+        disable_web_page_preview=True,
+        quote=True
+    )
+
