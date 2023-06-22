9213991174e27bdb60d1c47b0e1e3a911abed82e
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-22 08:30:37 +0530

    Create __init__.py

---
+++ AutoPost/__init__.py
@@ -0,0 +1,14 @@
+# (c) @TheLx0980
+# Year : 2023
+
+import logging
+from os import environ
+
+API_ID = environ.get('API_ID', '')
+API_HASH = environ.get('API_HASH', '')
+ADMIN_ID = environ.get('ADMIN_ID', '')
+SESSION = environ.get('SESSION', '')
+ 
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
