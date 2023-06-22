4840ffbce009ecdfb588899e57ad65188c7a6f1d
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-22 09:35:55 +0530

    Update post.py

--- AutoPost/plugins/post.py
+++ AutoPost/plugins/post.py
@@ -11,28 +11,32 @@ media_filter = filters.document | filters.video
 
 @Client.on_message(filters.channel & (media_filter))
 async def editing(bot, message):
-      try:
-         media = message.document or message.video or message.audio
-         caption_text = "@HQFilms4u"
-      except:
-         caption_text = ""
-         pass 
-      if (message.document or message.video or message.audio): 
-          if message.caption:                        
-             file_caption = f"**{message.caption}**"                
-          else:
-             fname = media.file_name
-             filename = fname.replace("_", ".")
-             file_caption = f"`{filename}`"  
-              
-      try:
-          if caption_position == "top":
-             await bot.edit_message_caption(
-                 chat_id=message.chat.id, 
-                 message_id=message.id,
-                 caption=file_caption + "\n" + caption_text,
-                 parse_mode=enums.ParseMode.MARKDOWN
-             )
+    try:
+        media = message.document or message.video or message.audio
+        caption_text = "@HQFilms4u"
+    except:
+        caption_text = ""
+        pass
+    if message.document or message.video or message.audio:
+        if message.caption:
+            file_caption = f"**{message.caption}**"
+        else:
+            fname = media.file_name
+            filename = fname.replace("_", ".")
+            file_caption = f"`{filename}`"
 
-      except:
-          pass
+        # Remove username and link from caption
+        file_caption = file_caption.replace("@MaxPlayHD", "").replace("👉", "")
+
+        try:
+            if caption_position == "top":
+                await bot.copy_message(
+                    chat_id=-1001986761426,
+                    from_chat_id=-1001921917995,
+                    message_id=update.id,
+                    caption=file_caption + '\n\n' + f"**{caption_text}**",
+                    parse_mode=enums.ParseMode.MARKDOWN
+                )
+
+        except:
+            pass
