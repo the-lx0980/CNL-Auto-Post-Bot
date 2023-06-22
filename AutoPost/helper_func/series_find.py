b88fc05ffe45642641a6a49bfd90071bdba81343
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-22 15:06:47 +0530

    Create series_find.py

---
+++ AutoPost/helper_func/series_find.py
@@ -0,0 +1,9 @@
+# (c) @TheLx0980
+# Year : 2023
+# Time : 14:53
+# After masturbation 😐
+
+series_block = []
+for season_number in range(1, 61):
+    season_string = f"S{str(season_number).zfill(2)}"
+    series_block.append(season_string)
