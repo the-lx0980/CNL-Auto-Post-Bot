0701a54c850566135a3fc710fda5deb00e61d780
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-22 08:52:38 +0530

    Update user.py

--- AutoPost/user.py
+++ AutoPost/user.py
@@ -1,16 +1,16 @@
 # (c) @TheLx0980
 # Year : 2023
 
-from . import Info, LOGGER
+from . import API_ID, API_HASH, SESSION, LOGGER
 from pyrogram import Client, __version__
 
-class Userbot(Client, Info):
+class Userbot(Client):
     def __init__(self):
         super().__init__(
             "userClient",
-            api_hash=self.API_HASH,
-            api_id=self.API_ID,
-            bot_token=self.SESSION,
+            api_hash=API_HASH,
+            api_id=API_ID,
+            bot_token=SESSION,
             workers=20,
             plugins={
                 "root": "AutoPost/plugins"
