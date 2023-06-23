35f8b0e07bfd7a48d13623bddd2dd838f659d8ee
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-06-23 09:01:55 +0530

    Update series_find.py

--- AutoPost/helper_func/series_find.py
+++ AutoPost/helper_func/series_find.py
@@ -3,11 +3,13 @@
 # Time : 15:31
 # After masturbation 😐
 
+
 series_block = [
     "S01", "S02", "S03", "S04", "S05", "S06", "S07", "S08", "S09", "S10",
     "S11", "S12", "S13", "S14", "S15", "S16", "S17", "S18", "S19", "S20",
     "S21", "S22", "S23", "S24", "S25", "S26", "S27", "S28", "S29", "S30",
     "S31", "S32", "S33", "S34", "S35", "S36", "S37", "S38", "S29", "S40",
 
-    "Series", "SERIES", "series", "Sample", "SAMPLE", "sample",
+    "Series", "SERIES", "series", "Sample", "SAMPLE", "sample", "Season",
+    "SEASON", "season", "HEVC"
 ]
