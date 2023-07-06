07569e159d193aa27a489285351159067fd37052
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-07 00:35:45 +0530

    Update commands.py

--- AutoPost/plugins/commands.py
+++ AutoPost/plugins/commands.py
@@ -1,16 +1,22 @@
-# (c) @TheLx0980
+# (c) Lx 0980
 
+from AutoPost.Chat_msg import ChatMSG
 from pyrogram import filters, Client
-
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @Client.on_message(filters.command("start") & filters.private & filters.incoming)
 async def start(client, message):
     await message.reply(
-        text="I'm Online",
+        text=ChatMSG.START_MSG,
         disable_web_page_preview=True,
-        quote=True
+        quote=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                InlineKeyboardButton("Help", callback_data="help"),
+                InlineKeyboardButton("How Does This Works?", callback_data="abt")
+            ]
+        )
     )
-
 
     
 
