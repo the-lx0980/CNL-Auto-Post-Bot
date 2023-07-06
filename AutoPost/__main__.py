9488a6ca430bf180a0598bb1e23edba9b792ba8f
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-07 03:21:55 +0530

    Update __main__.py

--- AutoPost/__main__.py
+++ AutoPost/__main__.py
@@ -2,7 +2,35 @@
 # Year : 2023
 # Language : Python 3
 
-from .user import UserBot
+import logging
+from pyrogram import Client, idle
+from pyromod import listen  # type: ignore
+from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
-app = UserBot()
-app.run()
+
+logging.basicConfig(level=logging.INFO, encoding="utf-8", format="%(asctime)s - %(levelname)s - \033[32m%(pathname)s: \033[31m\033[1m%(message)s \033[0m")
+
+app = Client(
+    "Auto-Post-Bot",
+    api_id=21288218,
+    api_hash="670deb1f30b648fbb4f744e406161942",
+    bot_token="5466682882:AAEd0W0HHOoKokOlOU2NCNJDb9Zk6_f0HnI", 
+    in_memory=True,
+    plugins={'root':'AutoPost.plugins'},
+)
+
+
+if __name__ == "__main__":
+    logging.info("Starting the bot")
+    try:
+        app.start()
+    except (ApiIdInvalid, ApiIdPublishedFlood):
+        raise Exception("Your API_ID/API_HASH is not valid.")
+    except AccessTokenInvalid:
+        raise Exception("Your BOT_TOKEN is not valid.")
+    uname = app.me.username
+    logging.info(f"@{uname} is now running!")
+    idle()
+    app.stop()
+    logging.info("Bot stopped. Alvida!")
+
