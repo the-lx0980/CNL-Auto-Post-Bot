40bd63466153d65ae632044485e3c5532f086326
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-07 01:24:06 +0530

    Update callback.py

--- AutoPost/plugins/callback.py
+++ AutoPost/plugins/callback.py
@@ -1,72 +1,56 @@
-# (c) Lx 0980
-
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import asyncio
 
 CAPTION_DATA = {}
 
+@Client.on_message(filters.private & filters.command("startt"))
+async def start(bot, message):
+    buttons = [[
+        InlineKeyboardButton('Set Caption', callback_data='setcaption')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply_text(
+        text="Welcome! Click the button below to set the caption.",
+        reply_markup=reply_markup
+    )
+
 @Client.on_callback_query()
-async def callback_data(bot, update: CallbackQuery):
+async def callback_handler(bot, update):
     query_data = update.data
     user_id = update.from_user.id
     
-    if query_data == "start":
-        buttons = [[            
-            InlineKeyboardButton('मदद ⚙', callback_data="help")
-        ]]
-    
-        reply_markup = InlineKeyboardMarkup(buttons)
-        
-        await update.message.edit_text(
-            f"Welcome {update.from_user.mention}",
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML,
-            disable_web_page_preview=True
-        )
-    
-    elif query_data == "help":
-        buttons = [[
-            InlineKeyboardButton('पीछे जाए ⚡', callback_data='start'),
-            InlineKeyboardButton('Set Caption', callback_data='caption')
-        ]]
-    
-        reply_markup = InlineKeyboardMarkup(buttons)
-        
-        caption = CAPTION_DATA.get(user_id)
-        caption_txt = caption.get("caption") if caption else "You have not set any caption"
-        
-        await update.message.edit_text(
-            f"<b>Caption</b>: {caption_txt}",
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML,
-            disable_web_page_preview=True
-        )
-    
-    elif query_data == "caption":
-        buttons = [[
-            InlineKeyboardButton('पीछे जाए ⚡', callback_data='start'),
-        ]]
-        
-        reply_markup = InlineKeyboardMarkup(buttons)
-        
-        await update.message.edit_text(
-            f"<b>Set Caption</b>: Send your desired caption as a separate message.",
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML,
-            disable_web_page_preview=True
+    if query_data == 'setcaption':
+        await bot.send_message(
+            chat_id=user_id,
+            text="Please send your desired caption within 30 seconds."
         )
 
-@Client.on_message(filters.text & ~filters.command)
+        try:
+            reply = await bot.listen(user_id, timeout=30)
+        except asyncio.TimeoutError:
+            await bot.send_message(
+                chat_id=user_id,
+                text="Caption not set. Timeout reached."
+            )
+        else:
+            if reply.text:
+                CAPTION_DATA[user_id] = reply.text
+                await bot.send_message(
+                    chat_id=user_id,
+                    text="Caption set successfully."
+                )
+            else:
+                await bot.send_message(
+                    chat_id=user_id,
+                    text="Invalid caption. Please try again."
+                )
+
+@Client.on_message(filters.text & filters.private)
 async def handle_text_message(bot, message):
     user_id = message.from_user.id
-    caption_data = CAPTION_DATA.get(user_id)
-    if caption_data:
-        caption = caption_data.get("caption")
-        await message.reply_text(
-            text=message.text,
-            caption=caption
-        )
+    caption = CAPTION_DATA.get(user_id)
+    if caption:
+        await message.reply_text(text=message.text, caption=caption)
     else:
-        await message.reply_text(
-            text=message.text
-        )
+        await message.reply_text(text=message.text)
