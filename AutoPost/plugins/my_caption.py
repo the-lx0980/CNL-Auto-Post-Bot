d91f773d70df6cf24b7e9e67075e3a9d942ca2ce
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-07 10:38:14 +0530

    Update my_caption.py

--- AutoPost/plugins/my_caption.py
+++ AutoPost/plugins/my_caption.py
@@ -1,7 +1,7 @@
 import logging
 import asyncio
 from pyromod import listen
-from AutoPost import app as Bot
+from AutoPost.bot import app as Bot
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram.errors import MessageNotModified
