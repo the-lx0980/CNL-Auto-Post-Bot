c6e9b52ce40918355d1fa24d662368b13709fbfb
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-12 21:42:00 +0530

    Update post.py

--- AutoPost/plugins/post.py
+++ AutoPost/plugins/post.py
@@ -18,29 +18,34 @@ db = Database()
 @Client.on_message(filters.channel & media_filter)
 async def editing(bot, message):
     from_chat_id = str(message.chat.id)
-    forwarding = await db.get_from_chat(from_chat_id)
+    forwarding = db.get_forwarding(from_chat_id)
     if forwarding:
         from_chat_id, to_chat_id = forwarding
 
         if message.document or message.video or message.audio:
             if message.caption:
                 file_caption = f"**{message.caption}**"
-        else:
-            return
-            
+            else:
+                return  # Skip processing if no caption is present
+
             if any(block in file_caption for block in series_block):
                 return
 
             text = file_caption
             file_caption = remove_content(text)
-        
-            await bot.copy_message(
+
+            caption = file_caption.strip()
+
+            if caption:
+                await bot.copy_message(
                     chat_id=to_chat_id,
                     from_chat_id=from_chat_id,
                     message_id=message.message_id,
                     caption=caption,
                     parse_mode=enums.ParseMode.MARKDOWN
                 )
+
+
 
 
 def remove_content(text):
