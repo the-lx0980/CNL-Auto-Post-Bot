3f341fec193b73129695ff4790574037b7159f58
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-12 21:46:03 +0530

    Create remove_txt.py

---
+++ AutoPost/helper_func/remove_txt.py
@@ -0,0 +1,4 @@
+def remove_content(text):
+    text = text.replace("@MaxPlayHD", "").replace("👉", "")
+    text = text.replace("Join Us On Telegram", "").replace("Support us", "").replace("🙏", "").replace("❤️", "")
+    return tex
