7012e24773139252cba591c1e53a35904de5f96b
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-12 21:06:41 +0530

    Update database.py

--- AutoPost/database/database.py
+++ AutoPost/database/database.py
@@ -6,33 +6,35 @@
 from pymongo import MongoClient
 
 class Database:
-    def __init__(self):
-        connection_string = "mongodb+srv://filesautobot:filesautobot870@cluster0.qcxdkpw.mongodb.net/?retryWrites=true&w=majority"
-        database_name = "auto post"
-        collection_name = "auto-post-collection"
-
+    def __init__(self, connection_string, database_name, collection_name):
         self.client = MongoClient(connection_string)
         self.db = self.client[database_name]
         self.collection = self.db[collection_name]
 
-    def add_forwarding(self, from_chat_id, to_chat_id):
-        # Check if the entry already exists
-        existing_entry = self.collection.find_one({"from_chat_id": from_chat_id, "to_chat_id": to_chat_id})
+    def add_forwarding(self, user_id, from_chat_id, to_chat_id):
+        existing_entry = self.collection.find_one({"user_id": user_id})
         if existing_entry:
             return False
-
         data = {
+            "user_id": user_id,
             "from_chat_id": from_chat_id,
             "to_chat_id": to_chat_id
         }
         self.collection.insert_one(data)
         return True
 
-    def get_forwarding(self):
-        # Retrieve all entries from the database
-        entries = self.collection.find({})
-        forwarding = []
-        for entry in entries:
-            forwarding.append((entry["from_chat_id"], entry["to_chat_id"]))
-        return forwarding
+    def get_forwarding(self, user_id):
+        entry = self.collection.find_one({"user_id": user_id})
+        if entry:
+            from_chat_id = entry["from_chat_id"]
+            to_chat_id = entry["to_chat_id"]
+            return (from_chat_id, to_chat_id)
+        return None
 
+    def remove_forwarding(self, user_id):
+        result = self.collection.delete_one({"user_id": user_id})
+        return result.deleted_count > 0
+
+    def remove_forwarding(self, user_id, from_chat_id, to_chat_id):
+        result = self.collection.delete_one({"user_id": user_id, "from_chat_id": from_chat_id, "to_chat_id": to_chat_id})
+        return result.deleted_count > 0
