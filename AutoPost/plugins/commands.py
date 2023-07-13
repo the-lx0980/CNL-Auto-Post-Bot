7117dbe04f2c93272b24dd47dd97209c3e7ef2e6
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-13 12:13:55 +0530

    Update commands.py

--- AutoPost/plugins/commands.py
+++ AutoPost/plugins/commands.py
@@ -12,3 +12,23 @@ async def check_from_chat(client, message):
         await message.reply_text(f"Your 'from' chat ID: {from_chat_id}\nTo Chat ID: {to_chat_id}")
     else:
         await message.reply_text("You haven't set a forwarding connection.")
+
+
+
+get_channels_for_user
+
+@Client.on_message(filters.command('forward_list'))
+def my_channel_command(client, message):
+    user_id = str(message.chat.id)
+    channels = db.get_channels_for_user(user_id)
+    if channels:
+        reply_text = ""
+        for idx, channel in enumerate(channels, 1):
+            from_chat_id = channel['from_chat_id']
+            to_chat_id = channel['to_chat_id']
+            reply_text += f"{idx}. From: {from_chat_id}\n   Target: {to_chat_id}\n\n"
+
+        message.reply_text(reply_text)
+    else:
+        reply_text = "No channels found for the user."
+        message.reply_text(reply_text)
