6d8cd3ca789cc8d490c9bf6c5d0b8fe7f8617e48
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-13 12:49:32 +0530

    Update database.py

--- AutoPost/database/database.py
+++ AutoPost/database/database.py
@@ -46,20 +46,18 @@ class Database:
 
     def get_channels_for_user(self, user_id):
         try:
-            channels = self.collection.find({'from_chat_id': user_id})
+            channels = self.collection.find({'user_id': user_id})
             return list(channels)
         except Exception as e:
             print("Error occurred while retrieving channels for user from the database:", str(e))
             return []
 
-
-    # for Blocking text messages
-    def add_block_text(self, from_chat_id, text):
-        existing_data = self.block_collection.find_one({'from_chat_id': from_chat_id})
+    def add_block_text(self, user_id, from_chat_id, text):
+        existing_data = self.block_collection.find_one({'user_id': user_id})
         if existing_data:
-            self.block_collection.update_one({'chat_id': chat_id}, {'$push': {'texts': text}})
+            self.block_collection.update_one({'from_chat_id': from_chat_id}, {'$push': {'texts': text}})
         else:
-            data = {'chat_id': chat_id, 'texts': [text]}
+            data = {'user_id': user_id, 'from_chat_id': from_chat_id, 'texts': [text]}
             self.block_collection.insert_one(data)
 
     def get_texts(self, from_chat_id):
@@ -68,11 +66,15 @@ class Database:
             return data['texts']
         return []
 
-    def delete_text(self, from_chat_id, text):
-        self.block_collection.update_one({'from_chat_id': from_chat_id}, {'$pull': {'texts': text}})
+    def delete_text(self, user_id, from_chat_id, text):
+        data = self.block_collection.find_one({'user_id': user_id})
+        if data:  
+            self.block_collection.update_one({'from_chat_id': from_chat_id}, {'$pull': {'texts': text}})
 
-    def delete_all_texts(self, from_chat_id):
-        self.block_collection.update_one({'from_chat_id': from_chat_id}, {'$set': {'texts': []}})
+    def delete_all_texts(self, user_id, from_chat_id):
+        data = self.block_collection.find_one({'user_id': user_id})
+        if data:    
+            self.block_collection.update_one({'from_chat_id': from_chat_id}, {'$set': {'texts': []}})
 
     def clear_database(self):
         try:
@@ -81,7 +83,7 @@ class Database:
         except Exception as e:
             print("Error occurred while clearing the database:", str(e))     
 
-    def delete_connection(self, from_chat_id, to_chat_id):
+    def delete_connection(self, user_id, from_chat_id, to_chat_id):
         try:
             existing = self.collection.find({'user_id': user_id})
             if existing:
