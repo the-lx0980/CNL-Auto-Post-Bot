a8f9a8e60c66c1e155cf1a63c86949252c28f42b
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-13 11:21:58 +0530

    Update database.py

--- AutoPost/database/database.py
+++ AutoPost/database/database.py
@@ -5,7 +5,8 @@ class Database:
         self.client = MongoClient("mongodb+srv://filesautobot:filesautobot870@cluster0.qcxdkpw.mongodb.net/?retryWrites=true&w=majority")
         self.db = self.client["auto-post"]
         self.collection = self.db["auto-post-collection"]
-  
+        self.block_collection = self.db["blocked-text"]
+    
     def save_chat_ids(self, from_chat_id, to_chat_id):
         try:
             existing_data = self.collection.find_one({'from_chat_id': from_chat_id, 'to_chat_id': to_chat_id})
@@ -42,3 +43,25 @@ class Database:
             return None
         except Exception as e:
             print("Error occurred while retrieving chat IDs from the database:", str(e))
+
+
+    # for  Blocking text messages 
+    def add_block_text(self, from_chat_id, text):
+        existing_data = self.block_collection.find_one({'from_chat_id': from_chat_id})
+        if existing_data:
+            self.block_collection.update_one({'chat_id': chat_id}, {'$push': {'texts': text}})
+        else:
+            data = {'chat_id': chat_id, 'texts': [text]}
+            self.block_collection.insert_one(data)
+
+    def get_texts(self, from_chat_id):
+        data = self.block_collection.find_one({'from_chat_id': from_chat_id})
+        if data:
+            return data['texts']
+        return []
+
+    def delete_text(self, from_chat_id, text):
+        self.block_collection.update_one({'from_chat_id': from_chat_id}, {'$pull': {'texts': text}})
+
+    def delete_all_texts(self, from_chat_id):
+        self.block_collection.update_one({'from_chat_id': from_chat_id}, {'$set': {'texts': []}})
