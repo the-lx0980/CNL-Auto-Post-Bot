54fffc9bb610a0aa8102db95ad0783a69bf16d68
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-13 23:12:30 +0530

    Update channels.py

--- AutoPost/plugins/channels.py
+++ AutoPost/plugins/channels.py
@@ -13,14 +13,17 @@ async def my_channel_command(client, message):
         if channels:
             buttons = []
             for channel in channels:
-                from_chat_id = channel['from_chat_id']  
+                from_chat_id = channel['from_chat_id']
                 chat = await client.get_chat(int(from_chat_id))
                 button = InlineKeyboardButton(chat.title, callback_data=f"managecl#{from_chat_id}")
                 buttons.append([button])
 
+            buttons.append([InlineKeyboardButton("Close", callback_data="close")])
+
             reply_markup = InlineKeyboardMarkup(buttons)
-            reply_text = "Your channels:"
+            reply_text = "Manage Your Channels:"  
             await client.send_message(chat_id=message.chat.id, text=reply_text, reply_markup=reply_markup)
+
         else:
             reply_text = "You have no channels."
             await client.send_message(chat_id=message.chat.id, text=reply_text)
