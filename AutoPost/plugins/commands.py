f42a42d6e9ed8f4eb92b3c95fd4d35aedc069dc8
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-13 07:56:33 +0530

    Update commands.py

--- AutoPost/plugins/commands.py
+++ AutoPost/plugins/commands.py
@@ -8,8 +8,7 @@ async def check_from_chat(client, message):
     from_chat_id = str(message.chat.id)
     entry = db.get_chat_ids(from_chat_id)
     if entry:
-        from_chat_id = entry["from_chat_id"] 
-        to_chat_id = entry["to_chat_id"]
-        await message.reply_text(f"Your 'from' chat ID: {from_chat_id}")
+        from_chat_id, to_chat_id = entry
+        await message.reply_text(f"Your 'from' chat ID: {from_chat_id}\nTo Chat ID: {to_chat_id}")
     else:
         await message.reply_text("You haven't set a forwarding connection.")
