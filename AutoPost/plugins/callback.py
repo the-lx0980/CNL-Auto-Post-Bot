f5f47faf44f596855ace25048adfb7b326cda575
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-13 23:55:35 +0530

    Update callback.py

--- AutoPost/plugins/callback.py
+++ AutoPost/plugins/callback.py
@@ -25,27 +25,33 @@ async def start(bot, message):
 async def callback_handler(client: Bot, cb: CallbackQuery):
     query_data = cb.data
     user_id = str(cb.message.chat.id)
+    
     if query_data == 'set_forward':
-        from_chat_id = await cb.message.chat.ask("Send me your 'from' Channel ID starting with -100:", parse_mode=enums.ParseMode.HTML)
-        from_chat_id = from_chat_id.text    
-        if not from_chat_id.isdigit() or not from_chat_id.startswith("-100"):
-            return await cb.message.reply_text("Invalid Channel ID. Please enter a valid 'from' Channel ID starting with -100.")
-        if len(from_chat_id) > 14:
+        channel_id = await cb.message.chat.ask("Send me your 'from' Channel ID starting with -100:", parse_mode=enums.ParseMode.HTML)
+        channel_id = from_chat_id.text.strip()
+        
+        if not channel_id.isdigit() or not from_chat_id.startswith("-100"):
+            return await cb.message.reply_text("Invalid Channel ID. Please enter a valid Channel ID")
+        
+        if len(channel_id) > 14:
             return await cb.message.reply_text("Invalid Chat ID...\nChat ID should be something like this: <code>-100xxxxxxxxxx</code>")
+        
         try:
-            chat = await client.get_chat(int(from_chat_id))
-        except:
-            return await cb.message.reply_text("Make sure Channel ID Is Vailed and Bot Admin In Your Channel")    
+            chat = await client.get_chat(int(channel_id))
+            if chat.type != enums.ChatType.CHANNEL:
+                return await cb.message.reply_text("This is not a valid channel ID. Please provide a channel ID.")
+            if not await client.get_chat_member(chat.id, "me"):
+                return await cb.message.reply_text("I am not an admin in the specified channel. Please make sure I am added as an admin.")
+        except Exception as e:
+            return await cb.message.reply_text(f"An error occurred while validating the channel ID: {str(e)}")
         channel_id = str(chat.id)
-        if chat.type != enums.ChatType.CHANNEL:
-            return await cb.message.reply_text("This Is Not Channel ID, I Only Support Channels") 
         db.save_chat_ids(user_id, channel_id)
         get_data = db.get_chat_ids(channel_id)
         if get_data:
-            from_chat_id, to_chat_id = get_data
-            await cb.message.reply_text(f"{chat.title} Is Successfully Added! Now Manege Your Channel From Bot PM")
+            await cb.message.reply_text("Channel successfully added. You can now manage your channel from the bot's private messages.")
         else:
-            await cb.message.reply_text("You Have Already Added This Channel.")
+            await cb.message.reply_text("You have already added this channel.")
+
         
     elif query_data.startswith("managecl"):
         try:
