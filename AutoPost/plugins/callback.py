6e31ff3f9379bdb4886f83b4f3e423f7087413a3
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-13 23:40:31 +0530

    Update callback.py

--- AutoPost/plugins/callback.py
+++ AutoPost/plugins/callback.py
@@ -27,21 +27,24 @@ async def callback_handler(client: Bot, cb: CallbackQuery):
     user_id = str(cb.message.chat.id)
     if query_data == 'set_forward':
         from_chat_id = await cb.message.chat.ask("Send me your 'from' Channel ID starting with -100:", parse_mode=enums.ParseMode.HTML)
-        from_chat_id = from_chat_id.text
-        if not from_chat_id.startswith("-100"):
-            return await cb.message.reply_text("Invalid Chat ID...\nChat ID should be something like this: <code>-100xxxxxxxxxx</code>")  
-        to_chat_id = await cb.message.chat.ask("Send me your 'to' Channel ID starting with -100:", parse_mode=enums.ParseMode.HTML)
-
-        if not to_chat_id.text.startswith("-100"):
+        from_chat_id = from_chat_id.text    
+        if not from_chat_id.isdigit() or not from_chat_id.startswith("-100"):
+            return await cb.message.reply_text("Invalid Channel ID. Please enter a valid 'from' Channel ID starting with -100.")
+        if len(from_chat_id) > 14:
             return await cb.message.reply_text("Invalid Chat ID...\nChat ID should be something like this: <code>-100xxxxxxxxxx</code>")
+        try:
+            chat = await client.get_chat(int(from_chat_id))
+        except:
+            return await cb.message.reply_text("Make sure Channel ID is Vailed and Bot admin in Channel")    
+        channel_id = str(chat.id)
         await client.send_message(chat_id=cb.message.chat.id, text=f"User ID: {user_id}\nSource ID: {from_chat_id}\nTarget ID: {to_chat_id}")
         db.save_chat_ids(user_id, from_chat_id, to_chat_id)
         get_data = db.get_chat_ids(from_chat_id)
         if get_data:
             from_chat_id, to_chat_id = get_data
-            await cb.message.reply_text(f"Forwarding set from `{from_chat_id}` to `{to_chat_id}`.")
+            await cb.message.reply_text(f"{chat.title} Is Successfully Added! Now Manege Your Channel From Bot PM")
         else:
-            await cb.message.reply_text("You have already set forwarding for your channel IDs.")
+            await cb.message.reply_text("You Have Already Added This Channel.")
         
     elif query_data.startswith("managecl"):
         try:
