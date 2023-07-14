60159f90c439e6a88a2e9a0c463d41a23d3773af
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-14 13:18:08 +0530

    Update auto_caption.py

--- AutoPost/helper_func/auto_edit/auto_caption.py
+++ AutoPost/helper_func/auto_edit/auto_caption.py
@@ -7,27 +7,24 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 logger = logging.getLogger(__name__)
 db = Database()
 
-
-
 @Client.on_callback_query()
 async def callback_handler(client: Bot, cb: CallbackQuery):
-  query_data = cb.data
-  user_id = str(cb.message.chat.id)
-  await set_channel(client, cb, query_data, user_id)
-
+    query_data = cb.data
+    user_id = str(cb.message.chat.id)
+    await set_channel(client, cb, query_data, user_id)
 
 
 async def set_channel(client, cb, query_data, user_id):
     if query_data == 'set_forward':
         channel_id = await cb.message.chat.ask("Send me your 'from' Channel ID starting with -100:", parse_mode=enums.ParseMode.HTML)
         channel_id = channel_id.text.strip()
-        
+
         if not channel_id.startswith("-100"):
-            return await cb.message.reply_text("Invalid Channel ID. Please enter a valid Channel ID With '-100'")
-        
+            return await cb.message.reply_text("Invalid Channel ID. Please enter a valid Channel ID with '-100'.")
+
         if len(channel_id) > 14:
-            return await cb.message.reply_text("Invalid Chat ID...\nChat ID should be something like this: <code>-100xxxxxxxxxx</code>")
-        
+            return await cb.message.reply_text("Invalid Chat ID...\nChat ID should be something like this: <code>-100xxxxxxxxxx</code>.")
+
         try:
             chat = await client.get_chat(int(channel_id))
             if chat.type != enums.ChatType.CHANNEL:
@@ -36,11 +33,11 @@ async def set_channel(client, cb, query_data, user_id):
                 return await cb.message.reply_text("I am not an admin in the specified channel. Please make sure I am added as an admin.")
         except Exception as e:
             return await cb.message.reply_text(f"An error occurred while validating the channel ID: {str(e)}")
-        
+
         channel_id = str(chat.id)
         db.save_channel_id(user_id, channel_id)
         get_data = db.get_channel_id(channel_id)
         if get_data:
             await cb.message.reply_text(f"{get_data} Channel successfully added. You can now manage your channel from the bot's private messages.")
         else:
-            await cb.message.reply_text("You have already added this channel."
+            await cb.message.reply_text("You have already added this channel.")
