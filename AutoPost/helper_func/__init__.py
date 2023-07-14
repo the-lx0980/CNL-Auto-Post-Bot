4ab4b4ba707ec781ae054c2c55c8c52bf2c07540
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-14 15:16:46 +0530

    Update __init__.py

--- AutoPost/helper_func/__init__.py
+++ AutoPost/helper_func/__init__.py
@@ -1,3 +1,4 @@
 from .series_find import series_block
 from .remove_txt import remove_content
-# from .cb_channel import channels_pagination_callback
+from .auto_edit.set_caption import set_auto_caps
+from .auto_edit.auto_edit_status import captin_status
