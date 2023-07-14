d52329f48e68789ea4b743fe17ce8ee536a7fc0b
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-14 15:06:24 +0530

    Update auto_edit_status.py

--- AutoPost/helper_func/auto_edit/auto_edit_status.py
+++ AutoPost/helper_func/auto_edit/auto_edit_status.py
@@ -5,7 +5,43 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 db = Database()
 
 
-async def set_channel(client, cb, query_data, _id):
+async def captin_stat(client, cb, query_data, _id):
     msg = cb.message
     channel_id = str(_id)
-    auto_cap = db.
+    get_cap = db.get_auto_cap(channel_id)
+    try:
+        chat = await client.get_chat(int(_id))
+    except:
+        return await msg.reply_text("I'm Not Admin In Your Channel")    
+    try:
+        if get_cap: 
+            buttons = [[
+                InlineKeyboardButton('Delete Caption', callback_data=f'auto_caption#{_id}')
+            ],[
+                InlineKeyboardButton('Back', callback_data=f'auto_caption#{_id}')
+            ]]
+            text = f"<b>Channel:</b> {chat.title}\n\nCaption:</b> {get_cap}"
+            await msg.edit_text(
+                chat_id=nsg.id,
+                text=text,
+                reply_markup=InlineKeyboardMarkup(buttons) 
+            )
+            return
+        
+        buttons = [[
+            InlineKeyboardButton('Set Caption', callback_data=f'set_caption#{_id}')
+        ],[
+            InlineKeyboardButton('Back', callback_data=f'auto_caption#{_id}')
+        ]]
+        text = f"<b>Channel:</b> {chat.title}\n\n<b>Caption:</b> You have not set any caption."
+        await msg.edit_text(
+            chat_id=nsg.id,
+            text=text,
+            reply_markup=InlineKeyboardMarkup(buttons) 
+        )
+    except Exception as e:
+        await msg.reply_text(f'Error - {e}')
+
+            
+            
+            
