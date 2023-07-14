78be7752c66c8da7db6b57d6d5346f2db62dd1b7
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-14 11:30:12 +0530

    Update __init__.py

--- AutoPost/helper_func/__init__.py
+++ AutoPost/helper_func/__init__.py
@@ -1,3 +1,3 @@
 from .series_find import series_block
 from .remove_txt import remove_content
-from .cb_channel import channels_pagination_callback
+# from .cb_channel import channels_pagination_callback
