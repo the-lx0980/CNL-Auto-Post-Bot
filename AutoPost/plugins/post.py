601e9720ad78d6359e2ba413d04229dad8461b05
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-14 23:38:02 +0530

    Update post.py

--- AutoPost/plugins/post.py
+++ AutoPost/plugins/post.py
@@ -3,16 +3,26 @@
 
 from AutoPost.helper_func import series_block
 from pyrogram import Client, filters, enums
+from AutoPost.database import Database 
+
+db = Database()
 
 @Client.on_message(filters.channel & (filters.document | filters.video))
 async def editing(bot, message):
     channel_id = str(message.chat.id)
-    get_data = get_caption(channel_id)
-    replaced = get_blocked(channel_id)
-#
+    get_data = db.get_caption(channel_id)
+    replacing = db.get_replace_data(channel_id)
     if get_data:
         m_caption, from_chat_id = get_data
         if message.caption:
+            media_caption = message.caption
+            if replaced:
+                for data in replacing:
+                    old_text = data['old_text']
+                    new_text = data['new_text']
+                    media_caption = media_caption.replace(old_text, new_text)
+                    if "##" in media_caption:
+                        media_caption = media_caption.replace("##", "")                                               
         else:
             return
         if blocked:   
@@ -21,9 +31,10 @@ async def editing(bot, message):
         text = media_caption
         file_caption = remove_content(text)
         caption = file_caption.strip()
+        caption = f"**{caption}\n\n{m_caption}**"
         await bot.copy_message(
             chat_id=-1001547532818,
-            from_chat_id=message.chat.id,
+            from_chat_id=int(from_chat_id),
             message_id=message.id,
             caption=caption,
             parse_mode=enums.ParseMode.MARKDOWN
