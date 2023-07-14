79b6c50b7f1530d4ff515335a36458f40627ee62
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-14 14:28:55 +0530

    Update auto_caption.py

--- AutoPost/helper_func/auto_edit/auto_caption.py
+++ AutoPost/helper_func/auto_edit/auto_caption.py
@@ -1,6 +1,6 @@
 from AutoPost.database import Database
 from pyrogram import enums
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 db = Database()
 
@@ -13,15 +13,14 @@ async def set_channel(client, cb, query_data, _id):
         channel_id = str(chat.id)
         get_cap = db.get_auto_cap(channel_id)
         if not get_cap:
-            cap_ask = msg.chat.ask("Now send Your Channel Caption",
-                                   parse_mode=enums.ParseMode.HTML)
+            cap_ask = await msg.chat.ask("Now send Your Channel Caption", parse_mode=enums.ParseMode.HTML)
             auto_caption = cap_ask.text
             if auto_caption:
                 await cap_ask.delete()
             db.set_auto_cap(channel_id, auto_caption)
             get_cap = db.get_auto_cap(channel_id)
             if get_cap:
-                edit_staus = f"<b>{chat.title}</b>\n\n<b>Caption:</b> {auto_caption}"
+                edit_status = f"<b>{chat.title}</b>\n\n<b>Caption:</b> {auto_caption}"
                 buttons = [[
                     InlineKeyboardButton('Delete Caption', callback_data=f'auto_caption#{_id}')
                 ],[
@@ -30,14 +29,10 @@ async def set_channel(client, cb, query_data, _id):
                 await query.answer(f"Your Caption Is Successfully Set!\n\n{auto_caption}", show_alert=True)
                 await client.send_message(
                     chat_id=msg.id,
-                    text=edit_staus
+                    text=edit_status,
                     reply_markup=InlineKeyboardMarkup(buttons)
+                )
             else:
                 await msg.reply_text("Your Caption Is Not set")
     except Exception as e:
         await msg.reply_text(f'Error - {e}')
-            
-            
-    
-    
-    
