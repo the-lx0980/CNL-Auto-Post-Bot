136c176cbe59f6ce50a523736d7206b4211d2e38
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-16 02:26:08 +0530

    Update commands.py

--- AutoPost/plugins/commands.py
+++ AutoPost/plugins/commands.py
@@ -7,8 +7,8 @@ db = Database()
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(client, message):
-    if str(message.chat.id) not in ADMINS:
-        return await message.reoly_text("This Bot is Not for Public")
+    if str(message.chat.id) != ADMINS:
+        return await message.reply_text("This Bot is Not for Public")
     text = """
 Welcome to AutoPost Bot!
 This bot allows you to manage your channels and automate the process of forwarding messages with customized captions.
