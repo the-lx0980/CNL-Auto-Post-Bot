cec04f1679cf126b8c09f4467ab06facc3e164bd
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-16 00:44:08 +0530

    Update __init__.py

--- AutoPost/__init__.py
+++ AutoPost/__init__.py
@@ -9,6 +9,7 @@ API_HASH = environ.get('API_HASH', '')
 ADMIN_ID = environ.get('ADMIN_ID', '')
 SESSION = environ.get('SESSION', '')
 ADMINS = environ.get('ADMINS', '').split(" ") 
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
