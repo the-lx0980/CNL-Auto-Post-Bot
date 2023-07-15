27c5664b94f7a9af4a3d927bb9c87332f5ea9e5f
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-16 00:35:30 +0530

    Update post.py

--- AutoPost/plugins/post.py
+++ AutoPost/plugins/post.py
@@ -28,7 +28,7 @@ async def editing(bot, message):
                         media_caption = media_caption.replace("##", "")                                               
             caption = media_caption.strip()
             caption = f"**{caption}\n\n{m_caption}**"
-            await bot.copy_message(
+            await bot.USER.copy_message(
                 chat_id=-1001988988405,
                 from_chat_id=int(from_chat_id),
                 message_id=message.id,
