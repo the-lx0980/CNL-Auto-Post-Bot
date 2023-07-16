9c8c4f423dd1c4b5cc25b8cdb5c3d11eb968859b
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-16 12:58:01 +0530

    Update commands.py

--- AutoPost/plugins/commands.py
+++ AutoPost/plugins/commands.py
@@ -20,8 +20,8 @@ To get started, use the following commands:
 • /delete_channel - Delete a channel from the database.
     format: (command) (channel id)
 • /add_replace_text - Add a replace text entry for a channel.
-    format: (command) (channel id) |!!| (old text) |!!| (new text)
-    seprate with "<code>|!!|</code>"
+    format: (command) (channel id) |:| (old text) |:| (new text)
+    seprate with "<code>|:|</code>"
 • /delete_replace_text - Delete a replace text entry for a channel.
     format: (command) (channel id) (old text)
 • /del_all_replace - Delete Replacing texts (Only bot admins)
@@ -86,17 +86,18 @@ async def delete_channel_command(client, message):
 async def add_replace_text_command(client, message):
     try:
         # Extract channel_id, old_text, and new_text from the command
-        command_parts = message.text.split("|!!|", 2)
+        command_parts = message.text.split("|:|", 2)
         if len(command_parts) != 3:
-            await message.reply_text("Invalid command format. Usage: /add_replace_text {channel_id} || {old_text} || {new_text}")
+            await message.reply_text("Invalid command format. Usage: /add_replace_text {channel_id} |:| {old_text} |:| {new_text}")
             return
 
         channel_id = command_parts[0].strip()
-        if not channel_id.startswith("-100"):
-            return await message.reply_text("Invalid Channel ID.")
+       # if not channel_id.startswith("-100"):
+     #       return await message.reply_text("Invalid Channel ID.")
         old_text = command_parts[1].strip()
         new_text = command_parts[2].strip()
-
+        await message.reply_text(f"{channel_id}\n{old_text}\n{new_text}")
+        return 
         db.save_replace_text(channel_id, old_text, new_text)
         await message.reply_text(f"<b>Old Text:</b> <code>{old_text}</code>\n<b>New Text:</b><code>{new_text}</code>\n\nReplace text added successfully.")
     except Exception as e:
