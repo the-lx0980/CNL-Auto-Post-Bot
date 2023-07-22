f20d1a18d02a6d5fea8c8cafc9aa2b9f6f70b411
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-22 15:41:04 +0530

    Update __init__.py

--- AutoPost/__init__.py
+++ AutoPost/__init__.py
@@ -11,7 +11,7 @@ id_pattern = re.compile(r'^.\d+$')
 API_ID = environ.get('API_ID', '2992000')
 API_HASH = environ.get('API_HASH', '235b12e862d71234ea222082052822fd')
 SESSION = environ.get('SESSION', '')
-ADMINS = [str(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
