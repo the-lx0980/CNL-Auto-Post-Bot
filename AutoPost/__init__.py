cc940bf07c8ee47e859d3783451166970cdf50bd
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-22 13:12:41 +0530

    Update __init__.py

--- AutoPost/__init__.py
+++ AutoPost/__init__.py
@@ -8,9 +8,8 @@ id_pattern = re.compile(r'^.\d+$')
 
 
 
-API_ID = environ.get('API_ID', '')
-API_HASH = environ.get('API_HASH', '')
-ADMIN_ID = environ.get('ADMIN_ID', '')
+API_ID = environ.get('API_ID', '2992000')
+API_HASH = environ.get('API_HASH', '235b12e862d71234ea222082052822fd')
 SESSION = environ.get('SESSION', '')
 ADMINS = [str(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
 
