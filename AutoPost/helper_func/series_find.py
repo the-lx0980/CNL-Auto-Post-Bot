ad25847cce39bed43259c417dd556cb1eacdce97
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-22 14:23:35 +0530

    Update series_find.py

--- AutoPost/helper_func/series_find.py
+++ AutoPost/helper_func/series_find.py
@@ -1,16 +1,18 @@
 # (c) @TheLx0980
 # Year : 2023
-# Time : 15:31
-# Month : Jun
-# After masturbation 😐
+# Time : 14:40
+# Month : July
+# Date : 22
 
 
-series_block = [
-    "S01", "S02", "S03", "S04", "S05", "S06", "S07", "S08", "S09", "S10",
-    "S11", "S12", "S13", "S14", "S15", "S16", "S17", "S18", "S19", "S20",
-    "S21", "S22", "S23", "S24", "S25", "S26", "S27", "S28", "S29", "S30",
-    "S31", "S32", "S33", "S34", "S35", "S36", "S37", "S38", "S29", "S40",
+from AutoPost.database import Database 
 
-    "Series", "SERIES", "series", "Sample", "SAMPLE", "sample", "Season",
-    "SEASON", "season", "HEVC", "10-Bit",
-]
+db = Database()  
+
+def contains_blocked_text(media_caption, channel_id):
+    blocked_texts = db.get_all_blocked_texts(channel_id)
+    if blocked_texts:
+        for blocked_text in blocked_texts:
+            if blocked_text in media_caption:
+                return True
+    return False
