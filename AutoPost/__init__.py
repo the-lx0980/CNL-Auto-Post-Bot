9c6862444ff33ff60746c40b0d8398dd9f685ec8
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-23 12:03:49 +0530

    Update __init__.py

--- AutoPost/__init__.py
+++ AutoPost/__init__.py
@@ -12,6 +12,7 @@ API_ID = environ.get('API_ID', '2992000')
 API_HASH = environ.get('API_HASH', '235b12e862d71234ea222082052822fd')
 SESSION = environ.get('SESSION', '')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
+DB_URL = environ.get('DB_URL', '')
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
