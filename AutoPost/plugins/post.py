d7a1c998f5ddb43623713a78a698d243815fef4e
IshwarRana <115400618+IshwarRana@users.noreply.github.com>
2024-12-26 07:47:22 +0530

    Update post.py

--- AutoPost/plugins/post.py
+++ AutoPost/plugins/post.py
@@ -27,7 +27,7 @@ async def editing(bot, message):
                     new_text = data['new_text']
                     media_caption = media_caption.replace(old_text, new_text)
                     if "##" in media_caption:
-                        media_caption = media_caption.replace("##", "")                                           
+                        media_caption = media_caption.replace("##", "").strip()  
             caption = media_caption.strip()
             if m_caption.strip() == '!()!':
                 caption = f"**{caption}**"          
@@ -37,7 +37,6 @@ async def editing(bot, message):
             caption = caption
         else:
             caption = message.caption
-        caption = caption.strip()
         try:           
             await bot.copy_message(
                 chat_id=int(to_chat),
